a24f126629744aa1597f7e91d5aac977d1d34a23
Michael Victor Zink <zuwiki@gmail.com>
2011-01-27 11:23:02 -0600

    Making test.rb slightly more readable and usable. Date is displayed before compilation/testing

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'fsevents'
 
 stream = FSEvents::Stream.watch('src') { |events|
-  print `echo ; echo "**** $(date) ****" ; erl -make ; erl -pa ebin -noinput -run telehash_test`
+  print `echo ; echo "**** $(date) ****"`
+  print `erl -make ; erl -pa ebin -noinput -run telehash_test`
 }
 stream.run
